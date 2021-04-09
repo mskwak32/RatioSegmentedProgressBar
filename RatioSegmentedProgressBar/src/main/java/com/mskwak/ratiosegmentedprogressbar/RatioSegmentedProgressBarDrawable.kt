f225e4beb4dfac175e625673d40db671c3e67bab
MSK <kase1@naver.com>
2021-04-09 23:58:06 +0900

    Ratio segmented progressbar drawable 추가

---
+++ RatioSegmentedProgressBar/src/main/java/com/mskwak/ratiosegmentedprogressbar/RatioSegmentedProgressBarDrawable.kt
@@ -0,0 +1,73 @@
+package com.mskwak.ratiosegmentedprogressbar
+
+import android.graphics.*
+import android.graphics.drawable.Drawable
+import androidx.annotation.ColorInt
+import androidx.annotation.DrawableRes
+
+class RatioSegmentedProgressBarDrawable(
+        @ColorInt private val foregroundColor: Int,
+        @ColorInt private val backgroundColor: Int,
+        divisionValueList: List<Long>,
+        maxValue: Long,
+        private val gapWidth: Float
+) : Drawable() {
+    private val numOfSegment: Int = divisionValueList.size
+    private val paint = Paint()
+    private val segment = RectF()
+    private val eachRatioList = mutableListOf<Float>()
+    private val cumulativeRatioList = mutableListOf<Float>()
+    private var sumOfSegmentWidth = 0f
+
+    init {
+        cumulativeRatioList.add(0f)
+        divisionValueList.forEachIndexed { index, value ->
+            val ratio = value / maxValue.toFloat()
+            eachRatioList.add(ratio)
+            cumulativeRatioList.add(cumulativeRatioList[index] + ratio)
+        }
+    }
+
+    override fun onBoundsChange(bounds: Rect?) {
+        super.onBoundsChange(bounds)
+        sumOfSegmentWidth = bounds!!.width() - (gapWidth * (numOfSegment - 1))
+    }
+
+    override fun onLevelChange(level: Int): Boolean {
+        invalidateSelf()
+        return true
+    }
+
+    override fun draw(canvas: Canvas) {
+        val level = level / 10000f
+        segment.set(0f, 0f, 0f, bounds.height().toFloat())
+        paint.color = foregroundColor
+
+        for (i in 0 until numOfSegment) {
+            val segmentWidth = sumOfSegmentWidth * eachRatioList[i]
+
+            segment.set(segment.left, 0f, segment.left + segmentWidth, segment.bottom)
+            val segmentLowLevel = cumulativeRatioList[i]
+            val segmentHighLevel = cumulativeRatioList[i + 1]
+            if (level in segmentLowLevel..segmentHighLevel) {
+                val middle = segment.left + (level - segmentLowLevel) * sumOfSegmentWidth
+                canvas.drawRect(segment.left, segment.top, middle, segment.bottom, paint)
+                paint.color = backgroundColor
+                canvas.drawRect(middle, segment.top, segment.right, segment.bottom, paint)
+            } else {
+                canvas.drawRect(segment, paint)
+            }
+
+            segment.offset(segment.width() + gapWidth, 0f)
+        }
+
+    }
+
+    override fun setAlpha(alpha: Int) {}
+
+    override fun getOpacity(): Int {
+        return PixelFormat.TRANSLUCENT
+    }
+
+    override fun setColorFilter(colorFilter: ColorFilter?) {}
+}
---
+++ app/src/main/java/com/mskwak/ratiosegmentedprogressbar/MainActivity.kt
@@ -0,0 +1,45 @@
+package com.mskwak.ratiosegmentedprogressbar
+
+import android.graphics.Color
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.LayoutInflater
+import androidx.lifecycle.Lifecycle
+import com.mskwak.ratiosegmentedprogressbar.databinding.ActivityMainBinding
+import kotlinx.coroutines.*
+
+class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+    private lateinit var progressJob: Job
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(LayoutInflater.from(this))
+        setContentView(binding.root)
+
+        initProgressBar()
+    }
+
+    override fun onPause() {
+        super.onPause()
+        progressJob.cancel()
+    }
+
+    private fun initProgressBar() {
+        val valueList = listOf(10L,20L,50L,90L,30L)
+        val maxValue = valueList.sum()
+        binding.progressBar.progressDrawable = RatioSegmentedProgressBarDrawable(Color.BLUE, Color.GRAY, valueList, maxValue, 20f)
+        binding.progressBar.max = maxValue.toInt()
+        progressJob = CoroutineScope(Dispatchers.Main).launch {
+            var progress = 0
+            while (progressJob.isActive) {
+                delay(200)
+                progress += 1
+                if(progress > maxValue) progress = 0
+                binding.progressBar.progress = progress
+                binding.progressText.text = progress.toString()
+            }
+        }
+    }
+}
